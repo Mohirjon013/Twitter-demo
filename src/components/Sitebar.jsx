2b63e9b9ebb8c40306ff1415a22b67d482ccd09e
Mohirjon013 <mohirjon26@gmail.com>
2024-09-27 04:03:13 +0100

    commit

---
+++ src/components/Sitebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Sitebar() {
+  return (
+    <div className='w-[25%]'>
+      sitbar
+    </div>
+  )
+}
+
+export default Sitebar
---
+++ src/routes/DashboardRoutes.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { Home, Profile } from '../pages'
+import Navbar from '../components/Navbar'
+import Sitebar from '../components/Sitebar'
+
+function Dashboard() {
+  return (
+    
+    <div className='flex '>
+      <Navbar />
+      <div className="w-[50%]">
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/profile' element={<Profile/>}/>
+        </Routes>
+      </div>
+      
+      <Sitebar/>
+    </div>
+  )
+}
+
+export default Dashboard
