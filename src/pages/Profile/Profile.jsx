240499091913cb8972aeb2bf25d297fed2200a34
Mohirjon013 <mohirjon26@gmail.com>
2024-10-02 08:07:22 +0100

    change

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -2,14 +2,38 @@ import React, { useState } from 'react'
 import BackIcon from "../../assets/images/back.svg"
 import BGInfo from "../../assets/images/bg-info.png"
 import PersonImg from "../../assets/images/profile-img.png"
-
+import Img from "../../assets/images/img.webp"
 import { Link, Outlet, useNavigate } from 'react-router-dom'
 import { Born, Date, Location } from '../../assets/images/Icons'
+import Button from "../../components/Button"
+import Model from '../../components/Model'
 function Profile() {
   const navigate = useNavigate()
   const user = JSON.parse(localStorage.getItem("token"))
-
+  const [ isUpdate , setIsUpdate] = useState(false)
   const [profileActive , setProfileActive] = useState("Tweets")
+  const [profileImg, setProfileImg] = useState(Img)
+
+  const [userInfo, setUserInfo] = useState({
+    avatarImg : profileImg,
+      name: JSON.parse(localStorage.getItem("token")).login,
+      email: "@Random_Email",
+      jobTitle: "UX&UI designer at",
+      companyName: "@abutechuz"
+  })
+  function handleUpdate(e){
+    e.preventDefault()
+    const data = {
+      avatarImg : profileImg,
+      name: e.target.userName.value,
+      email: e.target.userEmail.value,
+      jobTitle: e.target.info.value,
+      companyName: e.target.company.value,
+    }
+    setUserInfo(data)
+    setIsUpdate(false)
+  }
+
   return (
     <div className='h-[100vh] overflow-y-auto border-r-[1px] border-r-[#D8D8D8]'>
       <div className="flex gap-10 mt-[22px] pb-[15px] pl-[31px] border-b-[2px] border-b-[#CFCFCF]">
@@ -17,7 +41,7 @@ function Profile() {
           <img src={BackIcon} alt="back icon" width={20} />
         </button>
         <div className="space-y-[2px]">
-          <strong className='font-bold text-[20px] leading-[26px] block'>{user.login}</strong>
+          <strong className='font-bold text-[20px] leading-[26px] block'>{userInfo.name}</strong>
           <span className=' text-[16px] leading-[21px] opacity-60'>1,070 Tweets</span>
         </div>
       </div>
@@ -26,12 +50,12 @@ function Profile() {
         <div className=" rounded-full -mt-[75px]">
           <img className='w-[150px]  h-[150px]' src={PersonImg} alt="profile person img"  />
         </div>
-        <button type='button' className='w-[120px] mt-[20px] font-bold text-[18px]  border-[1px] border-[#999999] rounded-[50px]'>Edit profile</button>
+        <button onClick={() => setIsUpdate(true)} type='button' className='w-[120px] mt-[20px] font-bold text-[18px]  border-[1px] border-[#999999] rounded-[50px]'>Edit profile</button>
       </div>
       <div className="px-[25px]">
         <strong className='font-bold text-[24px] leading-[31px]'>{user.login.toUpperCase()}</strong>
-        <span className='block opacity-60 text-[16px] leading-[21px]'>@Random_Email</span>
-        <p className='mt-[15px] text-[16px] leading-[23px]'>UX&UI designer at <span className='text-[#1DA1F2]'>@abutechuz</span> </p>
+        <span className='block opacity-60 text-[16px] leading-[21px]'>{userInfo.email}</span>
+        <p className='mt-[15px] text-[16px] leading-[23px]'>{userInfo.jobTitle} <span className='text-[#1DA1F2]'>{userInfo.companyName}</span> </p>
       </div>
       <ul className="flex items-center gap-[15px] px-[25px] my-[15px]">
         <li className='flex items-center space-x-[5px]'>
@@ -64,6 +88,39 @@ function Profile() {
         <Link onClick={(e) => setProfileActive(e.target.textContent)} className={`${profileActive == "Likes" ? "before:left-0": ""} relative before:w-[100%] before:h-[4px] before:bg-blue-500 before:rounded-md before:absolute before:bottom-0 before:-left-[100%] duration-300 overflow-hidden font-bold text-[18px] pb-[19px] inline-block`} to={"likes"}>Likes</Link>
       </div>
       <Outlet/>
+      <Model isOpen={isUpdate} setIsOpen={setIsUpdate} >
+        <form onSubmit={handleUpdate} className='w-full'>
+          <label htmlFor="file">
+            <input onChange={(e) => setProfileImg(URL.createObjectURL(e.target.files[0]))} type="file" className='hidden' id='file' />
+            <img className='mx-auto rounded-lg' src={profileImg} alt="img" width={200} height={200} />
+          </label>
+        <div className="flex w-full justify-between mt-6">
+          <div className="w-[48%]  flex flex-col gap-[20px]">
+            <label className="flex flex-col">
+              <span className='text-[15px] text-black'>First name :</span>
+              <input className='bg-[#EEEEEE] placeholder:text-zinc-600 p-3 rounded-lg mt-2 outline-none focus:shadow  focus:shadow-gray-700' type="text" placeholder="Enter name " name="userName" autocomplete="off" required />
+            </label>
+            <label className="flex flex-col">
+              <span className='text-[15px] text-black'>User email:</span>
+              <input className='bg-[#EEEEEE] placeholder:text-zinc-600 p-3 rounded-lg mt-2 outline-none focus:shadow  focus:shadow-gray-700' type="email" placeholder="Enter email " name="userEmail" autocomplete="off" required />
+            </label>
+          </div>
+          <div className="w-[48%]  flex flex-col gap-[20px]">
+            <label className="flex flex-col">
+              <span className='text-[15px] text-black'>Job title :</span>
+              <input className='bg-[#EEEEEE] placeholder:text-zinc-600 p-3 rounded-lg mt-2 outline-none focus:shadow  focus:shadow-gray-700' type="text" placeholder="Enter Job title " name="info" autocomplete="off" required />
+            </label>
+            <label className="flex flex-col">
+              <span className='text-[15px] text-black'>User compny name :</span>
+              <input className='bg-[#EEEEEE] placeholder:text-zinc-600 p-3 rounded-lg mt-2 outline-none focus:shadow  focus:shadow-gray-700' type="text" placeholder="Enter compny name " name="company" autocomplete="off" required />
+            </label>
+          </div>
+        </div>
+
+        {/* <Button className="w-[400px] mt-5 p-2 text-[22px]">Update user</Button> */}
+        <button className=" mt-5 p-2 text-[22px] w-full bg-[#1DA1F2] rounded-[30px] text-white" type='submit' >Update user</button>
+        </form>
+      </Model>
     </div>
   )
 }
